9f1d329b3615d5482a278773127be85e6c7e759a
Eckhart Koeppen <eckhart.koppen@intel.com>
2015-05-12 12:35:35 +0300

    Fix CC1101 example

--- examples/cc110x/receiver.cpp
+++ examples/cc110x/receiver.cpp
@@ -1,25 +1,25 @@
+// #define OLIMEXINO
+
 #include <common.h>
-#include <debug.h>
 
 uint8_t packet[64];
 
-typedef DEBUG_T<true> DEBUG;
-
 int main(void)
 {
-	//HSE::init();
-	//PLL::init();
-	DEBUG::init();
+	HSE::init();
+	PLL::init();
 	SYSCLK::init();
 	POWER::init();
 	PORT_A::init();
 	PORT_B::init();
+	PORT_C::init();
 	LED_GREEN::set_high();
-	LED_RED::set_high();
+	LED_RED::set_low();
 	EXT_INTERRUPT::init();
 	SPI::init();
 	CC1101::reset();
 	CC1101::init(cc110x_default_init_values, ARRAY_COUNT(cc110x_default_init_values));
+	CC1101::start_rx();
 	LED_GREEN::set_low();
 	while (1) {
 		CC1101::template rx_buffer<TIMEOUT_NEVER>(packet, sizeof(packet));
